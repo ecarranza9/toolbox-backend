5f6c072c63fe00c432e866bd7579ddfa44fd8b15
ecarranza9 <eliancarranza9@gmail.com>
2022-03-18 12:45:53 -0300

    CORS agregados

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import router from './routes/index.js'
+import cors from 'cors'
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`)
 })
+
+app.use(cors())
 
 app.use(router)
 
